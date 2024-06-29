4da251cbd43e9abbd2ab1099c761ee1a1d5df241
gurleen5 <gk536742@gmail.com>
2024-06-29 20:40:23 +0530

    basicjavascript

---
+++ conditions.js
@@ -0,0 +1,13 @@
+n=342;
+if(n%2==0){
+    console.log('even');
+    something='hello';
+    let x='nice';
+    const pi=3.14;
+    //pi=345.345;
+}
+else{
+    console.log('odd');
+}
+console.log(something);
+//console.log(x);
---
+++ index.js
@@ -0,0 +1,16 @@
+console.log('hello javascript');
+console.log('javascript');
+a = 637;
+console.log(a);
+console.log(typeof a);
+myname= 'gurleen'
+console.log(myname);
+console.log(typeof myname);
+islog=true;
+console.log(islog);
+//exponent operator
+console.log(2**3);
+//datatype comapare
+console.log(1==='1');
+console.log(34+'45');
+console.log(11+'11'+11);
